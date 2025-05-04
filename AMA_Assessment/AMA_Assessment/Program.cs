090b9047a2a897e8469c3ca19c7adf86cce0ea76
Jacky Cheung <ting.fai.cheung@gmail.com>
2025-05-03 22:42:19 -0400

    modified the README.md and push the code to the repo.

---
+++ AMA_Assessment/AMA_Assessment/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AMA_Assessment
+{
+    class Handler {
+        public static String ShiftingString(String input, int numShift)
+        {
+            int size = input.Length;
+            StringBuilder result = new StringBuilder();
+            if(numShift % size == 0) //Case 1: if the number is same as the string size, then it can return the same string directly.
+            {
+                return input;
+            }
+            else //Case 2: if the number is not same as the string size, then it can start process the shifting.
+            {
+                int remainder = numShift % size; //Get the remainder that can avoid wasting resource to loop.
+                int startIndex = size - remainder;
+                result.Append(input.Substring(startIndex));
+                result.Append(input.Substring(0, startIndex));
+            }
+            return result.ToString(); //The return type is string, hence it has to use .ToString().
+        }
+        public static Boolean IsLetter(String input)
+        {
+            return Regex.IsMatch(input, @"^[a-zA-Z\s]+$"); //Check is the string only contains alphabet and whitespace.
+        }
+        public static Boolean IsVaildNumber(String input)
+        {
+            int temp;
+            bool result = int.TryParse(input, out temp); //Check is the string convertible to integer
+            Console.WriteLine("temp : " + temp);
+            if (!result || temp < 0)
+            {
+                return false;
+            }
+            return result;
+        }
+    }
+    class View
+    {
+        public static String ReadingFirstArg()
+        {
+            Console.WriteLine("Please enter the word \n Caution: It only accepts letters");
+            return Console.ReadLine();
+        }
+        public static String ReadingSecondArg()
+        {
+            Console.WriteLine("Please enter the number of shifting the word \n Caution: It only accepts zero or greater number");
+            return Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello Welcome to AMA assessment.");
+                int attemped = 5;
+                String arg1 = View.ReadingFirstArg();
+                while (!Handler.IsLetter(arg1))
+                {
+                    if (attemped < 1) break;
+                    attemped--;
+                    Console.WriteLine("The word you entered has contained non-letter character. Try it again.");
+                    Console.WriteLine(attemped + " attemp(s) remaining");
+                    arg1 = View.ReadingFirstArg();
+                }
+                String arg2 = View.ReadingSecondArg();
+                attemped = 5; //Reset to 5 attemps
+                while (!Handler.IsVaildNumber(arg2))
+                {
+                    if(attemped < 1) break;
+                    attemped--;
+                    Console.WriteLine("The number you entered is not vaild. Try it again.");
+                    Console.WriteLine(attemped + " attemp(s) remaining");
+                    arg2 = View.ReadingSecondArg();
+                }
+                int numArg2 = int.Parse(arg2);
+                Console.WriteLine(Handler.ShiftingString(arg1, numArg2));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# AMA_Assessment
+# AMA Assessment
+
+This is a C# console application, it's a assessment from a technical test.
+The application will ask the user to enter a word/sentence, and then a number, finally it will output a result.
+-The word only accpet **letter** and **whitespace**.
+-The number only **zero or greater**.
+-If the enter is not valid, the program will need the user retry.
+-Each enter will only have 5 attempts.
+The application will be shifting amount of characters position by the entered number.
+
+## For example:
+
+```
+Input1 = “abcdef”, Input2 = 2  Output = “efabcd”
+Input1 = “hello world”, Input2 = 5  Output = “ worldhello”
+```
+
+## How to run the applcation
